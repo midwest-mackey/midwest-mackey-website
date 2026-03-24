37f88c1c4036a59de72923cd509a4a961c601d77
Caleb Mackey <calebpatrickmackey@gmail.com>
2026-03-24 11:34:23 -0500

    fix: testing networking issue

--- src/app/shared/services/twitch.service.ts
+++ src/app/shared/services/twitch.service.ts
@@ -16,9 +16,6 @@ export class TwitchService {
     if (hostname === 'localhost') {
       // Dev on local machine
       this.apiUrl = 'http://localhost:3000/twitch/live';
-    } else if (hostname.includes('midwestmackey.com')) {
-      // Production public domain
-      this.apiUrl = 'https://api.midwestmackey.com/twitch/live';
     } else {
       // Likely running inside Docker Compose network
       this.apiUrl = 'http://twitch-api:3000/twitch/live';
