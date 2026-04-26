4b32a40e537e55256da865bbcfe045db6b0b7415
Caleb Mackey <calebpatrickmackey@gmail.com>
2026-04-25 22:08:06 -0500

    fix: localhost api & stats card

--- src/app/shared/services/api-endpoints.ts
+++ src/app/shared/services/api-endpoints.ts
@@ -2,19 +2,19 @@ import { environment } from 'src/environments/environment';
 
 export const API = {
   fortnite: {
-    profile: () => `${environment.apiBaseUrl}/fortnite/profile`
+    profile: () => `${environment.apiUrl}/fortnite/profile`
   },
 
   twitch: {
     live: (username: string) =>
-      `${environment.apiBaseUrl}/twitch/live/${username}`
+      `${environment.apiUrl}/twitch/live/${username}`
   },
 
   spotify: {
     nowPlaying: () =>
-      `${environment.apiBaseUrl}/spotify/now-playing`,
+      `${environment.apiUrl}/spotify/now-playing`,
 
     history: (limit: number) =>
-      `${environment.apiBaseUrl}/spotify/recently-played?limit=${limit}`
+      `${environment.apiUrl}/spotify/recently-played?limit=${limit}`
   }
 };
